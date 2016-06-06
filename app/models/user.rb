8a1eca685f7ee0369d4d5f9c876019c7a0fc3d08
Sneha Narasimhan <email": null>
2016-06-06 10:49:24 -0700

    Add template to register, login, logout.

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+require 'bcrypt'
+class User < ActiveRecord::Base
+  include BCrypt
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    if user.password == password
+      return user
+    else
+      nil
+    end
+  end
+end
